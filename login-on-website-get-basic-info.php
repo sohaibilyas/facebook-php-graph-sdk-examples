7e352b2e259f6fb66773f6e5dc4ab85e78531596
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-09-06 12:59:49 +0500

    Update login-on-website-get-basic-info.php
    graph error is fixed...

--- login-on-website-get-basic-info.php
+++ login-on-website-get-basic-info.php
@@ -3,13 +3,13 @@ session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
-  	'app_id' => 'APP_ID',
-  	'app_secret' => 'APP_SECRET',
-  	'default_graph_version' => 'v2.2',
-  	]);
+  'app_id' => 'APP_ID',
+  'app_secret' => 'APP_SECRET',
+  'default_graph_version' => 'v2.2',
+  ]);
 
 $helper = $fb->getRedirectLoginHelper();
-
+	
 try {
 	if (isset($_SESSION['facebook_access_token'])) {
 		$accessToken = $_SESSION['facebook_access_token'];
@@ -17,13 +17,13 @@ try {
   		$accessToken = $helper->getAccessToken();
 	}
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  	// When Graph returns an error
-  	echo 'Graph returned an error: ' . $e->getMessage();
+ 	// When Graph returns an error
+ 	echo 'Graph returned an error: ' . $e->getMessage();
   	exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
+ 	// When validation fails or other local issues
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
+  	exit;
  }
 
 if (isset($accessToken)) {
@@ -44,16 +44,17 @@ if (isset($accessToken)) {
 	}
 
 	try {
-		$response = $fb->get('/me');
-		$userNode = $response->getGraphUser();
+	$response = $fb->get('/me');
+	$userNode = $response->getGraphUser();
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		echo 'Graph returned an error: ' . $e->getMessage();
-		exit;
+	// When Graph returns an error
+	echo 'Graph returned an error: ' . $e->getMessage();
+	unset($_SESSION['facebook_access_token']);
+	exit;
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
+	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
 	}
 
 	echo 'Logged in as ' . $userNode->getName();
