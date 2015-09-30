3ad6930ef16d0548d12d3268b3e6c51cfa091317
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-09-30 11:56:31 +0500

    Update login-on-website-get-basic-info.php

--- login-on-website-get-basic-info.php
+++ login-on-website-get-basic-info.php
@@ -5,10 +5,12 @@ require_once __DIR__ . '/src/Facebook/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => 'APP_ID',
   'app_secret' => 'APP_SECRET',
-  'default_graph_version' => 'v2.2',
+  'default_graph_version' => 'v2.4',
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // optional
 	
 try {
 	if (isset($_SESSION['facebook_access_token'])) {
@@ -19,6 +21,7 @@ try {
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
  	// When Graph returns an error
  	echo 'Graph returned an error: ' . $e->getMessage();
+
   	exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
  	// When validation fails or other local issues
@@ -27,12 +30,11 @@ try {
  }
 
 if (isset($accessToken)) {
-
-	if(isset($_SESSION['facebook_access_token'])) {
+	if (isset($_SESSION['facebook_access_token'])) {
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	} else {
-	  	// Logged in!
-	  	$_SESSION['facebook_access_token'] = (string) $accessToken;
+		// getting short-lived access token
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
 
 	  	// OAuth 2.0 client handler
 		$oAuth2Client = $fb->getOAuth2Client();
@@ -40,30 +42,33 @@ if (isset($accessToken)) {
 		// Exchanges a short-lived access token for a long-lived one
 		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
 
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+		// setting default access token to be used in script
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	}
 
+	// getting basic info about user
 	try {
-	$response = $fb->get('/me');
-	$userNode = $response->getGraphUser();
+		$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+		$profile = $profile_request->getGraphNode()->asArray();
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
-	unset($_SESSION['facebook_access_token']);
-	exit;
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		unset($_SESSION['facebook_access_token']);
+		exit;
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
 	}
+	
+	// printing $profile array on the screen which holds the basic info about user
+	print_r($profile);
 
-	echo 'Logged in as ' . $userNode->getName();
-
-  	// Now you can redirect to another page and use the
-  	// access token from $_SESSION['facebook_access_token']
+  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
-	$permissions = ['email']; // optional
-	$loginUrl = $helper->getLoginUrl('http://www.sohaibilyas.com/index.php', $permissions);
-
+	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+	$loginUrl = $helper->getLoginUrl('https://sohaibilyas.com/fbapp/', $permissions);
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
