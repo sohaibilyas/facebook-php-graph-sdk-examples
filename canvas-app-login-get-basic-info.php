ca9af8b0a01e5974c529a52543ebbeadcc7a37d2
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-09-30 12:02:21 +0500

    Update canvas-app-login-get-basic-info.php

--- canvas-app-login-get-basic-info.php
+++ canvas-app-login-get-basic-info.php
@@ -6,13 +6,15 @@ $fb = new Facebook\Facebook([
   'app_id' => 'APP_ID',
   'app_secret' => 'APP_SECRET',
   'default_graph_version' => 'v2.4',
-  ]);
+]);
 
 $helper = $fb->getCanvasHelper();
-	
+
+$permissions = ['email']; // optionnal
+
 try {
 	if (isset($_SESSION['facebook_access_token'])) {
-		$accessToken = $_SESSION['facebook_access_token'];
+	$accessToken = $_SESSION['facebook_access_token'];
 	} else {
   		$accessToken = $helper->getAccessToken();
 	}
@@ -28,11 +30,10 @@ try {
 
 if (isset($accessToken)) {
 
-	if(isset($_SESSION['facebook_access_token'])) {
+	if (isset($_SESSION['facebook_access_token'])) {
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	} else {
-	  	// Logged in!
-	  	$_SESSION['facebook_access_token'] = (string) $accessToken;
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
 
 	  	// OAuth 2.0 client handler
 		$oAuth2Client = $fb->getOAuth2Client();
@@ -40,32 +41,51 @@ if (isset($accessToken)) {
 		// Exchanges a short-lived access token for a long-lived one
 		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
 
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	}
 
+	// validating the access token
 	try {
-	$response = $fb->get('/me');
-	$user = $response->getGraphUser();
+		$request = $fb->get('/me');
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
-	unset($_SESSION['facebook_access_token']);
-	echo "<script>window.top.location.href='https://apps.facebook.com/APP_NAMESPACE/'</script>";
-	exit;
+		// When Graph returns an error
+		if ($e->getCode() == 190) {
+			unset($_SESSION['facebook_access_token']);
+			$helper = $fb->getRedirectLoginHelper();
+			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+			echo "<script>window.top.location.href='".$loginUrl."'</script>";
+			exit;
+		}
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
 	}
 
-	echo $user['name'];
+	// getting basic info about user
+	try {
+		$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+		$profile = $profile_request->getGraphNode()->asArray();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		unset($_SESSION['facebook_access_token']);
+		echo "<script>window.top.location.href='https://apps.facebook.com/APP_NAMESPACE/'</script>";
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
 
-  	// Now you can redirect to another page and use the
-  	// access token from $_SESSION['facebook_access_token']
+	// priting basic info about user on the screen
+	print_r($profile);
+
+  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
 	$helper = $fb->getRedirectLoginHelper();
-	$permissions = ['email']; // optional
 	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-
 	echo "<script>window.top.location.href='".$loginUrl."'</script>";
 }
