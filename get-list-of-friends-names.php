1f82530b276effb39193cc1ecc7c7eaf3d186abc
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-10-17 13:02:40 +0500

    Create get-list-of-friends-names.php

---
+++ get-list-of-friends-names.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+require_once __DIR__ . '/src/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => 'APP_ID',
+  'app_secret' => 'APP_SECRET',
+  'default_graph_version' => 'v2.4',
+]);
+
+$helper = $fb->getCanvasHelper();
+
+$permissions = ['user_friends']; // optionnal
+
+try {
+	if (isset($_SESSION['facebook_access_token'])) {
+	$accessToken = $_SESSION['facebook_access_token'];
+	} else {
+  		$accessToken = $helper->getAccessToken();
+	}
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ 	// When Graph returns an error
+ 	echo 'Graph returned an error: ' . $e->getMessage();
+  	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+ 	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  	exit;
+ }
+
+if (isset($accessToken)) {
+	if (isset($_SESSION['facebook_access_token'])) {
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	} else {
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
+
+	  	// OAuth 2.0 client handler
+		$oAuth2Client = $fb->getOAuth2Client();
+
+		// Exchanges a short-lived access token for a long-lived one
+		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	}
+
+	// validating the access token
+	try {
+		$request = $fb->get('/me');
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		if ($e->getCode() == 190) {
+			unset($_SESSION['facebook_access_token']);
+			$helper = $fb->getRedirectLoginHelper();
+			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+			echo "<script>window.top.location.href='".$loginUrl."'</script>";
+		}
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	// get list of friends' names
+	try {
+		$requestFriends = $fb->get('/me/taggable_friends?fields=name&limit=100');
+		$friends = $requestFriends->getGraphEdge();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	// if have more friends than 100 as we defined the limit above on line no. 68
+	if ($fb->next($friends)) {
+		$allFriends = array();
+		$friendsArray = $friends->asArray();
+		$allFriends = array_merge($friendsArray, $allFriends);
+		while ($friends = $fb->next($friends)) {
+			$friendsArray = $friends->asArray();
+			$allFriends = array_merge($friendsArray, $allFriends);
+		}
+		foreach ($allFriends as $key) {
+			echo $key['name'] . "<br>";
+		}
+		echo count($allfriends);
+	} else {
+		$allFriends = $friends->asArray();
+		$totalFriends = count($allFriends);
+		foreach ($allFriends as $key) {
+			echo $key['name'] . "<br>";
+		}
+	}
+
+  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+} else {
+	$helper = $fb->getRedirectLoginHelper();
+	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+	echo "<script>window.top.location.href='".$loginUrl."'</script>";
+}
