5e8f489cfd20ce2bd4982ecce72d3a76abbf29e6
Sohaib Ilyas (Roomi) <roomiprogrammer@gmail.com>
2015-11-08 22:51:55 +0500

    single multiple posting in managed groupd file is added

---
+++ single-multiple-posting-managed-groups.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/src/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => 'APP_ID',
+  'app_secret' => 'APP_SECRET',
+  'default_graph_version' => 'v2.4',
+]);
+
+$helper = $fb->getCanvasHelper();
+
+$permissions = ['user_managed_groups', 'publish_actions']; // optionnal
+
+try {
+	if (isset($_SESSION['facebook_access_token'])) {
+	$accessToken = $_SESSION['facebook_access_token'];
+	} else {
+  		$accessToken = $helper->getAccessToken();
+	}
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ 	// When Graph returns an error
+ 	echo 'Graph returned an error: ' . $e->getMessage();
+  	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+ 	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  	exit;
+ }
+
+if (isset($accessToken)) {
+	if (isset($_SESSION['facebook_access_token'])) {
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	} else {
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
+
+	  	// OAuth 2.0 client handler
+		$oAuth2Client = $fb->getOAuth2Client();
+
+		// Exchanges a short-lived access token for a long-lived one
+		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	}
+
+	// redirect user back to app when page receives $_GET['code'] variable
+	if (isset($_GET['code'])) {
+		echo "<script>window.top.location.href='https://apps.facebook.com/APP_NAMESPACE';</script>";
+		exit;
+	}
+	
+	// validating the access token
+	try {
+		$request = $fb->get('/me');
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		if ($e->getCode() == 190) {
+			unset($_SESSION['facebook_access_token']);
+			$helper = $fb->getRedirectLoginHelper();
+			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+			echo "<script>window.top.location.href='".$loginUrl."'</script>";
+			exit;
+		}
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	// get list of groups managed by user
+	try {
+		$requestGroups = $fb->get('/me/groups');
+		$groups = $requestGroups->getGraphEdge()->asArray();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+  		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	// post in single group managed by user
+	foreach ($groups as $key) {
+		if ($key['name'] == 'Funny Demons') {
+			$groupId = $key['id'];
+		}
+	}
+
+	try {
+		$requestPost = $fb->post('/' . $groupId . '/feed', array('message' => 'this message field must come from user-end as Facebook strictly prohibits the pre-filled message field'));
+		$post = $requestPost->getGraphNode()->asArray();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+  		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	// get response of single posting
+	print_r($post);
+
+	// post in all groups managed by user
+	foreach ($groups as $key) {
+		try {
+			$requestMultiPost = $fb->post('/' . $key['id'] . '/feed', array('message' => 'this message field must come from user-end as Facebook strictly prohibitsf the pre-filled message field'));
+			$multiPost = $requestMultiPost->getGraphNode()->asArray();
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+			// When Graph returns an error
+			echo 'Graph returned an error: ' . $e->getMessage();
+	  		exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+			// When validation fails or other local issues
+			echo 'Facebook SDK returned an error: ' . $e->getMessage();
+			exit;
+		}
+	}
+	
+	// get response for multiple posting
+	print_r($multiPost);
+  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+} else {
+	$helper = $fb->getRedirectLoginHelper();
+	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+	echo "<script>window.top.location.href='".$loginUrl."'</script>";
+}
