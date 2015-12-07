69a026e5827482c265cca0402cdcd9a553fd20b9
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-12-07 22:08:37 +0500

    Create tag-friends.php

---
+++ tag-friends.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require_once __DIR__ . '/src/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => 'APP_ID',
+  'app_secret' => 'APP_SECRET',
+  'default_graph_version' => 'v2.5'
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+// app directory could be anything but website URL must match the URL given in the developers.facebook.com/apps
+define('APP_URL', 'http://sohaibilyas.com/fbapp/');
+
+$permissions = ['publish_actions', 'user_friends']; // optional
+	
+try {
+	if (isset($_SESSION['facebook_access_token'])) {
+		$accessToken = $_SESSION['facebook_access_token'];
+	} else {
+  		$accessToken = $helper->getAccessToken();
+	}
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ 	// When Graph returns an error
+ 	echo 'Graph returned an error: ' . $e->getMessage();
+
+  	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+ 	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  	exit;
+ }
+
+if (isset($accessToken)) {
+	if (isset($_SESSION['facebook_access_token'])) {
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	} else {
+		// getting short-lived access token
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
+
+	  	// OAuth 2.0 client handler
+		$oAuth2Client = $fb->getOAuth2Client();
+
+		// Exchanges a short-lived access token for a long-lived one
+		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+		// setting default access token to be used in script
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	}
+
+	// redirect the user back to the same page if it has "code" GET variable
+	if (isset($_GET['code'])) {
+		header('Location: ./');
+	}
+
+	// validating user access token
+	try {
+		$user = $fb->get('/me');
+		$user = $user->getGraphNode()->asArray();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		session_destroy();
+		// if access token is invalid or expired you can simply redirect to login page using header() function
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+	
+	// getting all friends of user
+	$friends = $fb->get('/me/taggable_friends');
+	$friends = $friends->getGraphEdge()->asArray();
+
+	// getting random friend out of all friends
+	$totalFriends = count($friends);
+	$random = rand(0, $totalFriends);
+
+	// posting on facebook and tagging friend with it
+	$post = $fb->post('/me/feed', array('message' => 'my message', 'tags' => $friends[$random]['id']));
+
+  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+} else {
+	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+	$loginUrl = $helper->getLoginUrl(APP_URL, $permissions);
+	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+}
